01fe4996ec1df54ebbd01cefc0649345c04b66e2
Ed <houedward@gmail.com>
2021-05-04 12:15:20 -0700

    Added model and amplify boilerplate

--- app/src/main/java/com/edhou/taskmaster/TaskmasterApplication.kt
+++ app/src/main/java/com/edhou/taskmaster/TaskmasterApplication.kt
@@ -1,10 +1,23 @@
 package com.edhou.taskmaster
 
 import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
 import com.edhou.taskmaster.db.AppDatabase
 import com.edhou.taskmaster.db.TasksRepository
+
 
 class TaskmasterApplication : Application() {
     val database by lazy { AppDatabase.getInstance(this) }
     val repository by lazy { TasksRepository(database.tasksDao()) }
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            Amplify.configure(applicationContext)
+            Log.i("MyAmplifyApp", "Initialized Amplify")
+        } catch (e: AmplifyException) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", e)
+        }
+    }
 }
